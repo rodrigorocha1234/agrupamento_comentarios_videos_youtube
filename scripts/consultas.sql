f9220f07b35fb8e3ca88bf27560d1c2553858217
rodrigo <silva.rodrigo31@gmail.com>
2026-05-31 21:58:19 -0300

    fix: correção tipagem

--- scripts/consultas.sql
+++ scripts/consultas.sql
@@ -7,6 +7,9 @@ SET s3_secret_access_key='minio123';
 SET s3_use_ssl=false;
 SET s3_url_style='path';
 
+SELECT DISTINCT   snippet.channelId as id_canal,  snippet.videoId as id_video , nome_canal, titulo_video
+            FROM read_json_auto('s3://youtube/bronze/comentarios/id_canal=*/id_video=*/id_comentario=*/comentario*.json');
+
 
 SELECT *
 FROM read_json_auto('s3://youtube/bronze/comentarios/id_canal=*/id_video=*/id_comentario=*/comentario*.json');
